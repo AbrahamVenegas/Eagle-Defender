614c0992288a1c242885e8b16e77a5aef885fb02
Tavo121 <galvarado13@estudiantec.cr>
2023-10-04 19:56:38 -0600

    Validation of player 1 and 2 credentials

--- LogInWindow.py
+++ LogInWindow.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+import requests
+from button import Button
 
 
 def GetFont(size):
@@ -23,27 +25,34 @@ class LogInWindow:
         self.inputRect2 = pygame.Rect(15, 350, 350, 50)
         self.inputRect3 = pygame.Rect(415, 200, 350, 50)
         self.inputRect4 = pygame.Rect(415, 350, 350, 50)
-        self.inputRect5 = pygame.Rect(15, 450, 350, 50)
         self.p1UsernameRect = self.p1PasswordRect = None
         self.p2UsernameRect = self.p2PasswordRect = None
         self.color_passive = pygame.Color(178, 183, 191)
-        self.username = "Username:"
+        self.username = "Email:"
         self.password = "Password:"
+        self.inputColor = pygame.Color((178, 183, 191))
+        self.LogIn1 = self.LogIn2 = False
         self.baseFont = pygame.font.Font(None, 25)
         background = pygame.image.load("assets/LogInBG.png")
         pygame.display.set_caption("Eagle Defender")
 
         while True:
             self.screen.blit(background, (0, 0))
-            pygame.draw.line(surface=self.screen, start_pos=(self.width/2, 0), end_pos=(self.width/2, self.height), color=(0, 0, 0), width=5)
+            pygame.draw.line(surface=self.screen, start_pos=(self.width / 2, 0), end_pos=(self.width / 2, self.height),
+                             color=(0, 0, 0), width=5)
 
             self.p1UsernameRect = pygame.draw.rect(self.screen, self.color_passive, self.inputRect1, 3)
             self.p1PasswordRect = pygame.draw.rect(self.screen, self.color_passive, self.inputRect2, 3)
             self.p2UsernameRect = pygame.draw.rect(self.screen, self.color_passive, self.inputRect3, 3)
             self.p2PasswordRect = pygame.draw.rect(self.screen, self.color_passive, self.inputRect4, 3)
-            self.logInButton = pygame.draw.rect(self.screen, self.color_passive, self.inputRect5)
             self.Player1LogIn()
             self.Player2LogIn()
+
+            back = Button(image=None, pos=(740, 550),
+                              textInput="BACK", font=GetFont(25), baseColor="White", hoveringColor="Green")
+
+            back.ChangeColor(self.mousePos)
+            back.UpdateScreen(self.screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -65,14 +74,20 @@ class LogInWindow:
                         self.p2UsernameFlag = True
                     else:
                         self.p2UsernameFlag = False
-                        
+
                     if self.p2PasswordRect.collidepoint(event.pos):
                         self.p2PasswordFlag = True
                     else:
                         self.p2PasswordFlag = False
 
-                    if self.logInButton.collidepoint(event.pos):
-                        print("hola")
+                    if self.logInButton1.CheckForInput(self.mousePos) and not self.LogIn1:
+                        self.VerifyLogIn(player1=True, player2=False)
+
+                    if self.logInButton2.CheckForInput(self.mousePos) and not self.LogIn2:
+                        self.VerifyLogIn(player2=True, player1=False)
+
+                    '''if back.CheckForInput(self.mousePos):
+                        MainScreen()'''
 
                 if event.type == pygame.KEYDOWN:
                     if self.p1UsernameFlag:
@@ -103,9 +118,6 @@ class LogInWindow:
                             self.p2Password += event.unicode
                             self.hiddenPassword2 += '*'
 
-                    if event.key == pygame.KMOD_ALT:
-                        print("hola")
-
             pygame.display.update()
 
     def Player1LogIn(self):
@@ -113,30 +125,81 @@ class LogInWindow:
         logInText = GetFont(40).render("PLAYER 1", True, (0, 0, 0))
         usernameText = GetFont(20).render(self.username, True, (0, 0, 0))
         passwordText = GetFont(20).render(self.password, True, (0, 0, 0))
-        self.screen.blit(logInText, (15+25, 10+40))
+        self.screen.blit(logInText, (15 + 25, 10 + 40))
         self.screen.blit(usernameText, (15, 170))
         self.screen.blit(passwordText, (15, 320))
 
-        usernameSurface = self.baseFont.render(self.p1Username, True, (178, 183, 191))
-        self.screen.blit(usernameSurface, (self.p1UsernameRect.x+5, self.p1UsernameRect.y+15))
+        usernameSurface = self.baseFont.render(self.p1Username, True, self.inputColor)
+        self.screen.blit(usernameSurface, (self.p1UsernameRect.x + 5, self.p1UsernameRect.y + 15))
 
-        passwordSurface = self.baseFont.render(self.hiddenPassword1, True, (178, 183, 191))
-        self.screen.blit(passwordSurface, (self.p1PasswordRect.x+5, self.p1PasswordRect.y+15))
+        passwordSurface = self.baseFont.render(self.hiddenPassword1, True, self.inputColor)
+        self.screen.blit(passwordSurface, (self.p1PasswordRect.x + 5, self.p1PasswordRect.y + 15))
+
+        if not self.LogIn1:
+            self.mousePos = pygame.mouse.get_pos()
+            self.logInButton1 = Button(image=None, pos=(180, 450),
+                          textInput="LOG IN", font=GetFont(30), baseColor="Black", hoveringColor="Green")
+            self.logInButton1.ChangeColor(self.mousePos)
+            self.logInButton1.UpdateScreen(self.screen)
+        else:
+            Text1 = GetFont(30).render("LOGGED IN", True, (0, 0, 0))
+            self.screen.blit(Text1, (50, 440))
+
+        if usernameSurface.get_width() > self.p1UsernameRect.w:
+            self.p1Username = self.p1Username[:-1]
+        elif passwordSurface.get_width() > self.p1PasswordRect.w:
+            self.p1Password = self.p1Password[:-1]
+
 
     def Player2LogIn(self):
         self.screen.blit(self.rect, (415, 10))
         logInText = GetFont(40).render("PLAYER 2", True, (0, 0, 0))
         usernameText2 = GetFont(20).render(self.username, True, (0, 0, 0))
         passwordText2 = GetFont(20).render(self.password, True, (0, 0, 0))
-        self.screen.blit(logInText, (400+15+25, 10+40))
+        self.screen.blit(logInText, (400 + 15 + 25, 10 + 40))
         self.screen.blit(usernameText2, (415, 170))
         self.screen.blit(passwordText2, (415, 320))
 
-        usernameSurface2 = self.baseFont.render(self.p2Username, True, (178, 183, 191))
-        self.screen.blit(usernameSurface2, (self.p2UsernameRect.x+5, self.p2UsernameRect.y+15))
+        usernameSurface2 = self.baseFont.render(self.p2Username, True, self.inputColor)
+        self.screen.blit(usernameSurface2, (self.p2UsernameRect.x + 5, self.p2UsernameRect.y + 15))
 
-        passwordSurface = self.baseFont.render(self.hiddenPassword2, True, (178, 183, 191))
-        self.screen.blit(passwordSurface, (self.p2PasswordRect.x+5, self.p2PasswordRect.y+15))
+        passwordSurface = self.baseFont.render(self.hiddenPassword2, True, self.inputColor)
+        self.screen.blit(passwordSurface, (self.p2PasswordRect.x + 5, self.p2PasswordRect.y + 15))
 
+        if not self.LogIn2:
+            self.mousePos = pygame.mouse.get_pos()
+            self.logInButton2 = Button(image=None, pos=(400+180, 450),
+                          textInput="LOG IN", font=GetFont(30), baseColor="Black", hoveringColor="Green")
+            self.logInButton2.ChangeColor(self.mousePos)
+            self.logInButton2.UpdateScreen(self.screen)
+        else:
+            Text1 = GetFont(30).render("LOGGED IN", True, (0, 0, 0))
+            self.screen.blit(Text1, (450, 440))
 
-LogInWindow()
+        if usernameSurface2.get_width() > self.p2UsernameRect.w:
+            self.p2Username = self.p2Username[:-1]
+        elif passwordSurface.get_width() > self.p2PasswordRect.w:
+            self.p2Password = self.p2Password[:-1]
+
+    def VerifyLogIn(self, player1, player2):
+        url = "http://127.0.0.1:5000/api/login"
+        headers = {'Content-Type': 'application/json'}
+        if player1:
+            data = {
+                "email": self.p1Username,
+                "password": self.p1Password
+            }
+            self.LogIn1 = True
+        elif player2:
+            data = {
+                "email": self.p2Username,
+                "password": self.p2Password
+            }
+            self.LogIn2 = True
+
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 201:
+            print("Logged in")
+
+if __name__ == "__main__":
+    LogInWindow()
---
+++ StartWindow.py
@@ -0,0 +1,188 @@
+import pygame
+import sys
+import json
+from button import Button
+from LogInWindow import LogInWindow
+
+
+pygame.init()  # starts it
+Screen = pygame.display.set_mode((1440, 810))  # To set the parameters of the window
+pygame.display.set_caption("Eagle Defender")  # Window Title
+Background = pygame.image.load("assets/Background.png")  # Sets teh background of the Main Screen
+
+
+def GetFont(size):  # To return it in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
+
+def StartScreen():
+    while True:
+        pygame.display.set_caption("Inicio de Sesión") # To change the title of the window
+        playMousePosition = pygame.mouse.get_pos()
+        # To obtain the position of the mouse, this will be changed for the joystick later
+        Screen.fill("black")
+
+        playText = GetFont(45).render("This is the LOG IN screen.", True, "White")
+        playRectangle = playText.get_rect(center=(720, 260))
+        # To add text to the screen
+        Screen.blit(playText, playRectangle)
+
+        playBack = Button(image=None, pos=(720, 460),
+                           textInput="BACK", font=GetFont(75), baseColor="White", hoveringColor="Green")
+        # Button Escape to go back to the last screen
+        playBack.ChangeColor(playMousePosition)
+        playBack.UpdateScreen(Screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if playBack.CheckForInput(playMousePosition):
+                    MainScreen()
+
+        pygame.display.update()
+
+def RegisterScreen():
+    while True:
+
+        """
+        data = {
+            'name': 'abraham',
+            'password': '1234',
+            'email': 'abravenegas@estudiantec.cr',
+            'age': '20',
+            'song': 'torero'
+        }
+        
+
+        with open('register_data.text','w') as register_file:
+            json.dump(data, register_file)
+        """
+        pygame.display.set_caption("Registro")
+        registerMousePosition = pygame.mouse.get_pos()
+
+        Screen.fill("white")
+
+        registerText = GetFont(45).render("This is the REGISTER screen.", True, "Black")
+        registerRectangle = registerText.get_rect(center=(720, 260))
+        Screen.blit(registerText, registerRectangle)
+
+        registerBack = Button(image=None, pos=(50,20),
+                               textInput="BACK", font=GetFont(20), baseColor="Black", hoveringColor="Green")
+
+        registerBack.ChangeColor(registerMousePosition)
+        registerBack.UpdateScreen(Screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if registerBack.CheckForInput(registerMousePosition):
+                    MainScreen()
+
+        pygame.display.update()
+
+def LeaderboardScreen():
+    while True:
+        pygame.display.set_caption("Salón de la Fama")
+        leaderboardMousePosition = pygame.mouse.get_pos()
+
+        Screen.fill("white")
+
+        leadearboardText = GetFont(45).render("This is the LEADERBOARD screen.", True, "Black")
+        leadearboardRectangle = leadearboardText.get_rect(center=(720, 260))
+        Screen.blit(leadearboardText, leadearboardRectangle)
+
+        leadearboardBack = Button(image=None, pos=(720, 460),
+                                  textInput="BACK", font=GetFont(75), baseColor="Black", hoveringColor="Green")
+
+        leadearboardBack.ChangeColor(leaderboardMousePosition)
+        leadearboardBack.UpdateScreen(Screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if leadearboardBack.CheckForInput(leaderboardMousePosition):
+                    MainScreen()
+
+        pygame.display.update()
+
+def HelpScreen():
+    while True:
+        pygame.display.set_caption("Ayuda")
+        helpMousePosition = pygame.mouse.get_pos()
+
+        Screen.fill("black")
+
+        helpText = GetFont(45).render("This is the HELP screen.", True, "White")
+        helpRectangle = helpText.get_rect(center=(720, 260))
+        Screen.blit(helpText, helpRectangle)
+
+        helpBack = Button(image=None, pos=(720, 460),
+                           textInput="BACK", font=GetFont(75), baseColor="White", hoveringColor="Green")
+
+        helpBack.ChangeColor(helpMousePosition)
+        helpBack.UpdateScreen(Screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if helpBack.CheckForInput(helpMousePosition):
+                    MainScreen()
+
+        pygame.display.update()
+
+def MainScreen():
+    while True:
+        Screen.blit(Background, (0, 0))
+
+        menuMousePosition = pygame.mouse.get_pos()
+
+        menuText = GetFont(50).render("EAGLE DEFENDER", True, "#b68f40")
+        menuRectangle = menuText.get_rect(center=(720, 100))
+
+        playButton = Button(image=pygame.image.load("assets/MediumRectangle.png"), pos=(720, 215),
+                            textInput="START", font=GetFont(50), baseColor="#d7fcd4", hoveringColor="Purple")
+        registerButton = Button(image=pygame.image.load("assets/BigRectangle.png"), pos=(720, 340),
+                                textInput="¡REGISTER!", font=GetFont(50), baseColor="#d7fcd4", hoveringColor="Purple")
+        leaderboardButton = Button(image=pygame.image.load("assets/BigRectangle.png"), pos=(720, 465),
+                                   textInput="LEADERBOARD", font=GetFont(50), baseColor="#d7fcd4", hoveringColor="Purple")
+        helpButton = Button(image=pygame.image.load("assets/MediumRectangle.png"), pos=(720, 590),
+                            textInput="HELP", font=GetFont(50), baseColor="#d7fcd4", hoveringColor="Purple")
+        quitButton = Button(image=pygame.image.load("assets/QuitRectangle.png"), pos=(720, 715),
+                            textInput="QUIT", font=GetFont(50), baseColor="#d7fcd4", hoveringColor="Purple")
+        # Set the button for each Screen
+
+        Screen.blit(menuText, menuRectangle)
+
+        for button in [playButton, registerButton, leaderboardButton, quitButton, helpButton]:
+            button.ChangeColor(menuMousePosition)
+            button.UpdateScreen(Screen)
+        # Changes the color when the mouse is on top of the text
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if playButton.CheckForInput(menuMousePosition):
+                    LogIn = LogInWindow()
+                if registerButton.CheckForInput(menuMousePosition):
+                    RegisterScreen()
+                if leaderboardButton.CheckForInput(menuMousePosition):
+                    LeaderboardScreen()
+                if helpButton.CheckForInput(menuMousePosition):
+                    HelpScreen()
+                if quitButton.CheckForInput(menuMousePosition):
+                    pygame.quit()
+                    sys.exit()
+                # When the text is clicked it goes to the specific page
+        pygame.display.update()
+
+
+MainScreen()
