7971caa9d1508a79378536a9d913cd0fef4336f9
Albert Vega camacho <85046754+AlbertVega@users.noreply.github.com>
2023-10-26 01:26:25 -0600

    bloques casi totalmente implementados
    solo falta la vida de los bloques

--- classes/ConcreteBlock.py
+++ classes/ConcreteBlock.py
@@ -5,26 +5,33 @@ from classes.Block import Block
 
 
 class ConcreteBlock(Block, ABC):
-    sprite = pygame.image.load("assets/Concrete.png")
+    sprite = None
     type = None
-    BlockX = BlockY = 0
-    bulletX = bulletY = 0
-    rect = sprite.get_rect()
+    rect = None
 
     def __init__(self, BlockX, BlockY, surface):
         self.type = "Concrete"
         self.BlockX = BlockX
         self.BlockY = BlockY
         self.screen = surface
+        self.sprite = pygame.image.load("assets/Concrete.png")
+        self.rect = self.sprite.get_rect()
+        self.flag = False
+        self.SetPosition()
 
-    def SetPosition(self, mouseX, mouseY):
-        row = (mouseY // 32)
-        column = (mouseX // 32)
+    def SetPosition(self):
+        row = (self.BlockY // 32)
+        column = (self.BlockX // 32)
         if 8 <= row <= 10 and 3 <= column <= 6:
-            pass
+            self.flag = False
         elif 3 < row < 15 and column > 1:
             self.rect.x = column * 32
             self.rect.y = row * 32
+            self.flag = True
 
     def DrawBlock(self):
-        self.screen.blit(self.sprite, self.rect)
+        if self.flag:
+            self.screen.blit(self.sprite, self.rect)
+
+    def isCollision(self, object):
+        return self.rect.colliderect(object)
