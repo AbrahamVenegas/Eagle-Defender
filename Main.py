6a83685c7ea7ad63f18c35296419af9cef3b39d3
Tavo121 <galvarado13@estudiantec.cr>
2023-11-13 16:35:28 -0600

    main update

--- Main.py
+++ Main.py
@@ -1,13 +1,5 @@
-from graphics.StartWindow import StartWindow
-from graphics.LogInWindow import LogInWindow
-from graphics.HelpWindow import HelpWindow
-from graphics.RegisterWindow import RegisterWindow
-from graphics.GameWindow import GameWindow
+from classes.Mediator import Mediator
 
 if __name__ == "__main__":
-    gameWindow = GameWindow()
-    helpWindow = HelpWindow()
-    logInWindow = LogInWindow()
-    registerWindow = RegisterWindow()
-    startWindow = StartWindow(helpWindow, gameWindow, registerWindow)
-    startWindow.MainScreen()
+    mediator = Mediator()
+    mediator.run()
